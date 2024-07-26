726832bb4cc5d028a7df7aa22025fa41b03cda2e
illiyyin <iilliyyin@gmail.com>
2024-07-26 20:18:07 +0700

    add footer

--- src/component/shared/Footer.jsx
+++ src/component/shared/Footer.jsx
@@ -1,7 +1,49 @@
 import React from 'react'
+import style from './Footer.module.css'
+import logoWhite from '../../assets/Logo-White.png'
+import phoneIcon from '../../assets/PHONE-ICON.svg'
+import mailIcon from '../../assets/MAIL-ICON.svg'
 
 export default function Footer() {
-  return (
-    <div>Footer</div>
-  )
+	return (
+		<footer>
+			<div className={style['footer-container']}>
+				<div className={style['footer-icon']}>
+					<img
+						className={style['footer-logo']}
+						src={logoWhite}
+						alt=''
+					/>
+					<div>
+						<img className={style['mail']} src={mailIcon} alt='' />
+						<img
+							className={style['phone']}
+							src={phoneIcon}
+							alt=''
+						/>
+					</div>
+				</div>
+				<div className={style['footer-product']}>
+					<a className={style['footer-nav']} href='menu.html'>
+						Products
+					</a>
+					<p className={style['footer-nav-list']}>Coffee Based</p>
+					<p className={style['footer-nav-list']}>Non-Coffee</p>
+					<p className={style['footer-nav-list']}>Tea Based</p>
+				</div>
+				<div className={style['footer-store']}>
+					<a className={style['footer-nav']} href='findus.html'>
+						Store
+					</a>
+					<p className={style['footer-nav-list']}>Location</p>
+				</div>
+				<div className={style['footer-contact']}>
+					<a className={style['footer-nav']} href='help.html'>
+						Contact
+					</a>
+					<p className={style['footer-nav-list']}>Customer Service</p>
+				</div>
+			</div>
+		</footer>
+	)
 }
--- src/component/shared/Layout.jsx
+++ src/component/shared/Layout.jsx
@@ -1,15 +1,15 @@
 import { Outlet } from 'react-router-dom'
-import Navbar from "./Navbar"
-import Footer from "./Footer"
+import Navbar from './Navbar'
+import Footer from './Footer'
 
 const Layout = () => {
-  return (
-    <div>
-      <Navbar/>
-      <Outlet />
-      <Footer/>
-    </div>
-  )
+	return (
+		<div style={{ width: '100%' }}>
+			<Navbar />
+			<Outlet />
+			<Footer />
+		</div>
+	)
 }
 
 export default Layout
